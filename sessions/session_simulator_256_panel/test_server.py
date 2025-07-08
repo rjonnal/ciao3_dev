ed39880c7032955f61c2adfd2c3db2fd62b4a31d
Ravi S. Jonnal <rjonnal@gmail.com>
2025-07-08 16:55:28 -0700

    working panel sensor prototype in simulation

--- sessions/session_simulator_256_panel/test_server.py
+++ sessions/session_simulator_256_panel/test_server.py
@@ -5,57 +5,137 @@ from ciao3_dev.components import sensors,loops
 from ciao3_dev.components import sensors,loops
 import sys,os
 from matplotlib import pyplot as plt
+import numpy as np
 
-#pn.extension()
+
+css = '''
+.bk.panel-widget-box {
+font-size: 10px;
+}
+.bk-input-group {
+font-size: 10px;
+}
+.bk-input {
+font-size: 10px;
+}
+.bk-btn {
+font-size: 10px;
+}
+
+'''
+
+pn.extension(raw_css=[css])
+
+pn.extension(raw_css=[css])
 from streamz import Stream
 
-pn.extension('vega')
+#pn.extension('vega')
+#pn.extension('ipywidgets')
+
+# Set up sensor
+cam = simulator.Simulator()
+sensor = sensors.Sensor(cam)
+sensor.remove_tip_tilt = True
+sensor.sense()
+
+error_buffer = [0]*100
+
+# Set up sensor figure
+sensor_figure = plt.figure(figsize=(4,3))
+[[ax1,ax2],[ax3,ax4]] = sensor_figure.subplots(2,2)
+ax1.set_title('spots')
+ax1.set_xticks([])
+ax1.set_yticks([])
+
+ax2.set_title('wavefront')
+ax2.set_xticks([])
+ax2.set_yticks([])
+
+ax3.set_title('zernike coefficients ($\mu m$)')
+ax3.set_xlabel('index')
+
+ax4.set_title('wavefront error ($\mu m$ RMS)')
+ax4.set_xlabel('iteration')
+
+iterations = list(np.arange(-99,1))
+
+h1 = ax1.imshow(sensor.image,clim=(0,4095),cmap='gray')
+h2 = ax2.imshow(sensor.wavefront,cmap='jet')
+zidx = np.arange(len(sensor.zernikes))
+h3 = ax3.plot(zidx,sensor.zernikes,'ks',markersize=2,linestyle='none')
+h4 = ax4.plot(iterations,error_buffer,'b-')
 
 
 # Widgets
-exposure_time_input = pn.widgets.IntInput(name='Exposure Time (ms)', value=20)
+exposure_time_input = pn.widgets.IntInput(name='Exposure Time (ms)', value=150)
 background_adjustment_input = pn.widgets.IntInput(name='Background Adjustemnt (ADU)', value=0)
-start_button = pn.widgets.Button(name='Start', button_type='primary')
-pause_button = pn.widgets.Button(name='Pause', button_type='primary')
-result_display = pn.pane.Markdown("### Result: None")
+run_toggle = pn.widgets.button.Toggle(name='Run', button_type='primary')
+download_button = pn.widgets.Button(name='Download data', button_type='primary')
+calibrate_button = pn.widgets.Button(name='Record reference', button_type='primary')
+mpl_pane = pn.pane.Matplotlib(sensor_figure, dpi=150)
 
 def increment(x):
     return x + 1
 
-source = Stream()
-
-streamz_pane = pn.pane.Streamz(source.map(increment), always_watch=True)
-
 # Callback
-def on_start(event):
+count = 0
+
+def on_download(event):
     pass
 
-def on_pause(event):
-    pass
+download_button.on_click(on_download)
 
-start_button.on_click(on_start)
-pause_button.on_click(on_pause)
-
-count = 1
 def emit_count():
-    global count
-    count += 1
-    result_display.object = f"### Result: {count}"
-    source.emit(count)
+    if not run_toggle.value:
+        return
+    
+    global count,sensor,error_buffer,h1,h2,h3,h4,sensor_figure,mpl_pane
+    global ax1,ax2,ax3,ax4,iterations
 
-pn.state.add_periodic_callback(emit_count, period=500, count=999);
+
+    
+    sensor.cam.set_exposure(1000*exposure_time_input.value)
+    sensor.background_correction = background_adjustment_input.value
+    sensor.sense()
+
+    iterations = iterations[1:]
+    iterations.append(iterations[-1]+1)
+    
+    error_buffer = error_buffer[1:]
+    error_buffer.append(sensor.error*1e6)
+
+    h1.set_data(sensor.image-background_adjustment_input.value)
+    h2.set_data(sensor.wavefront)
+
+    zernikes = sensor.zernikes*1e6
+    h3[0].set_ydata(zernikes)
+    ax3.set_ylim((np.min(zernikes),np.max(zernikes)))
+
+    h4[0].set_xdata(iterations)
+    h4[0].set_ydata(error_buffer)
+    ax4.set_ylim((np.min(error_buffer),np.max(error_buffer)))
+    ax4.set_xlim((iterations[0],iterations[-1]))
+    
+    #sensor_figure.canvas.draw()
+    #sensor_figure.canvas.flush_events()
+    mpl_pane.object = sensor_figure
+    count += 1
+
+pn.state.add_periodic_callback(emit_count, period=100, count=999);
 
 
 # Layout
-app = pn.Column(pn.pane.Markdown("# CIAO Wavefront Sensor"),
-                pn.Row(pn.Column(exposure_time_input,
-                                 background_adjustment_input,
-                                 start_button,
-                                 pause_button,
-                                 width=400),
-                       pn.Column(result_display,
-                                 streamz_pane,
-                                 width=400),
-                       width=800))
+
+app = pn.Row(pn.Column(pn.pane.Markdown("**CIAO Wavefront Sensor**"),
+                       exposure_time_input,
+                       background_adjustment_input,
+                       run_toggle,
+                       download_button,
+                       calibrate_button,
+                       width=300,
+                       css_classes=['panel-widget-box']),
+             pn.Column(mpl_pane,
+                       width=900),
+             )
 #servable
 app.servable()
